bfa31d85a4f67b5d7d72e366ef811a0543f35051
Ademir Ferreira <whoisademir@gmail.com>
2022-03-30 12:29:35 -0300

    Introdução aos testes utilizando FACT e THEORY

---
+++ 01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(2, 2);
+            // Assert
+            Assert.Equal(4, resultado);
+        }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        [InlineData(4,2,6)]
+        [InlineData(6,2,8)]
+        [InlineData(8,2,10)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+            //Assert
+            Assert.Equal(total, resultado);
+        }
+    }
+}
