4b75e48e6a605ea22d3f36b6aa8f561a0f7ba2c4
Ademir Ferreira <whoisademir@gmail.com>
2022-03-31 11:47:28 -0300

    Simulando retorno de métodos com Mock

--- 01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
+++ 01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Bogus;
 using Bogus.DataSets;
 using Features.Clientes;
@@ -10,22 +12,31 @@ namespace Features.Tests._04___Dados_Humanos
     public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture>{}
     public class ClienteTestsBogusFixture : IDisposable
     {
-        public Cliente GerarClienteValido()
+        public Cliente GerarClienteValido() => GerarClientes(1, true).FirstOrDefault();
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+            return clientes;
+        }
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
         {
             var genero = new Faker().PickRandom<Name.Gender>();
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente(
                     Guid.NewGuid(),
                     f.Name.FirstName(genero),
                     f.Name.LastName(genero),
                     f.Date.Past(80, DateTime.Now.AddYears(-18)),
                     "",
-                    true,
+                    ativo,
                     DateTime.Now
                 )).RuleFor(c => c.Email, (f, c) =>
                     f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
 
-            return cliente;
+            return clientes.Generate(quantidade);
         }
 
         public Cliente GerarClienteInValido()
