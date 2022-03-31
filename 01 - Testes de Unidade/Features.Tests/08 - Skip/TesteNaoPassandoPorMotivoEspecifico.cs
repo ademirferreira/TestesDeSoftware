f45832e1b404c14bfd48ddb60cedece76136831b
Ademir Ferreira <whoisademir@gmail.com>
2022-03-31 14:35:49 -0300

    Escapando testes

---
+++ 01 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoPorMotivoEspecifico.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Features.Tests._08___Skip
+{
+    public class TesteNaoPassandoPorMotivoEspecifico
+    {
+        [Fact(DisplayName = "Novo Cliente 2.0", Skip = "Nova versão 2.0 quebrando")]
+        [Trait("Categoria", "Escapando Testes")]
+        public void Teste_NaoEstaPasando_VersaoNovaNaoCompativel()
+        {
+            Assert.True(false);
+        }
+    }
+}
